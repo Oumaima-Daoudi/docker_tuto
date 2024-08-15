a7baffb90ce670b26de13df81366595ec734ee98
Oumaima-Daoudi <daoudioumaima300@gmail.com>
2024-08-15 20:51:56 +0100

    docker with redis

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const redis = require('redis');
+
 
 // init app
 const PORT = 4000;
 const app = express();
+
+// connect to redis 
+const REDIS_PORT = 6379;
+const REDIS_HOST = 'redis'
+const redisClient = redis.createClient({
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+});
+redisClient.on('error', err => console.log('Redis Client Error', err));
+redisClient.on('connect', () => console.log('Connected to redis'));
+redisClient.connect();
 
 // connect db
 const DB_USER = 'root';
@@ -16,6 +28,14 @@ mongoose
   .then(() => console.log("database coonnected successful"))
   .catch((err) => console.log("connection failed :", err));
 
-app.get("/", (req, res) => res.send("<h1>hello oumaima ridaa</h1>"));
+app.get("/", (req, res) => {
+    redisClient.set('products','products....')
+    res.send("<h1>hello oumaima ridaa</h1>")
+});
+
+app.get("/data", async (req, res) => {
+    const products = await redisClient.get('products')
+    res.send(`<h1>hello oumaima ridaa</h1> <h2>${products}</h2>`)
+});
 
 app.listen(PORT, () => console.log(`app is up and running on port : ${PORT}`));
