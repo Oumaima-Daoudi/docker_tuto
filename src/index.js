cb3f7c469e5e8b568dad898992983ba3601bf9e6
Oumaima-Daoudi <daoudioumaima300@gmail.com>
2024-08-16 15:16:11 +0100

    docker with postgresql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const redis = require('redis');
+const { Client } = require('pg')
 
 
 // init app
@@ -20,22 +21,35 @@ redisClient.connect();
 // connect db
 const DB_USER = 'root';
 const DB_PASSWORD = 'example';
-const DB_PORT = 27017;
-const DB_HOST = 'mongo'
-const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-mongoose
-  .connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("database coonnected successful"))
-  .catch((err) => console.log("connection failed :", err));
+const DB_PORT = 5432;
+const DB_HOST = 'postgres'
+const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+const client = new Client({
+    connectionString:URI,
+  })
+client.connect()
+  .then(() => console.log("postgres database coonnected successful"))
+  .catch((err) => console.log("connection failed to postgres db :", err));
+
+
+// const DB_USER = 'root';
+// const DB_PASSWORD = 'example';
+// const DB_PORT = 27017;
+// const DB_HOST = 'mongo'
+// const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+// mongoose
+//   .connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("database coonnected successful"))
+//   .catch((err) => console.log("connection failed :", err));
 
 app.get("/", (req, res) => {
     redisClient.set('products','products....')
-    res.send("<h1>hello oumaima ridaa</h1>")
+    res.send("<h1>hello oumaima</h1>")
 });
 
 app.get("/data", async (req, res) => {
     const products = await redisClient.get('products')
-    res.send(`<h1>hello oumaima ridaa</h1> <h2>${products}</h2>`)
+    res.send(`<h1>hello oumaima</h1> <h2>${products}</h2>`)
 });
 
 app.listen(PORT, () => console.log(`app is up and running on port : ${PORT}`));
